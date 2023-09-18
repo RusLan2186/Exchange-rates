825fb47b212d1cbcb0ebf9ca2fc46fac3e79db1a
Ruslan <ruslanchikbest@gmail.com>
2023-09-18 16:14:32 +0300

    Add search

--- src/components/Info/OtherCurrency.js
+++ src/components/Info/OtherCurrency.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const OtherCurrency = ({ currencyList }) => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const searchResult = currencyList.filter((item) =>
+    item.txt.toLowerCase().includes(searchValue.toLowerCase()),
+  );
+
   return (
     <div>
-      {currencyList.map((currency) => (
+      <div>
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          type='text'
+          placeholder='Search....'
+        />
+      </div>
+      {searchResult.map((currency) => (
         <div key={currency.cc}>
           {currency.txt} {currency.rate} {currency.exchangedate}
         </div>
