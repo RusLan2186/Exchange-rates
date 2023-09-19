380d2fbcaf32cfe366c9a3c24b48ea66d69580ee
Ruslan <ruslanchikbest@gmail.com>
2023-09-19 15:45:19 +0300

    create component Convert

---
+++ src/components/Convert/Convert.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Convert = () => {
+  const corrency = ['ua', 'usa', 'eur', [pln]];
+  return (
+    <div>
+      <h1>Convert</h1>
+      <button>convert</button>
+    </div>
+  );
+};
+
+export default Convert;
