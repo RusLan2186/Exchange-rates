b532655f05aec813333523c04747d9c2a504caa7
Ruslan <ruslanchikbest@gmail.com>
2023-09-19 14:03:18 +0300

    Add styles to Other Currentcy

--- src/components/Info/OtherCurrency.js
+++ src/components/Info/OtherCurrency.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import cl from './OtherCurrency.module.scss';
 
 const OtherCurrency = ({ currencyList }) => {
   const [searchValue, setSearchValue] = useState('');
@@ -8,20 +9,28 @@ const OtherCurrency = ({ currencyList }) => {
   );
 
   return (
-    <div>
+    <div className={cl.wrapper}>
       <div>
         <input
+          className={cl.input}
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
           type='text'
           placeholder='Search....'
         />
       </div>
-      {searchResult.map((currency) => (
-        <div key={currency.cc}>
-          {currency.cc} {currency.rate}
+      {searchResult.length > 0 ? (
+        <div className={cl.container}>
+          {searchResult.map((currency) => (
+            <div className={cl.item} key={currency.cc}>
+              <span className={cl.cc}> {currency.cc} </span>
+              <span className={cl.rate}> {currency.rate}</span>
+            </div>
+          ))}
         </div>
-      ))}
+      ) : (
+        <h1 className={cl.not__found}> Currency not found</h1>
+      )}
     </div>
   );
 };
