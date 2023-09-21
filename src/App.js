61d8b6c5a06d6ee0873db0bc5d8430b9cc6e6ff1
Ruslan <ruslanchikbest@gmail.com>
2023-09-21 15:52:26 +0300

    Create routing, create header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Info from './components/Info/Info';
 import Container from '@mui/material/Container';
 
 import Logics from './components/Convertor/Logics';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className='wrapper'>
+      <Header />
       <Container>
-        <Info />
-        <Logics />
+        <Routes>
+          <Route path='/home' element={<Main />}></Route>
+        </Routes>
       </Container>
     </div>
   );
